f9dccfaac3732741197d2fa4218b56814c25d123
Fernando Mendoza <famendoza@gmail.com>
2024-08-05 15:11:14 -0400

    Finished Founder Buttons application.

--- FounderButtons/ViewController.swift
+++ FounderButtons/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNameLabel: UILabel!
+    
+    @IBOutlet weak var secondNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func googleFounderButtonPressed(_ sender: UIButton) {
+        firstNameLabel.text = "Larry Page"
+        secondNameLabel.text = "Sergey Brin"
+    }
+    
+    @IBAction func rentTheRunwayButtonPressed(_ sender: UIButton) {
+        firstNameLabel.text = "Jennifer Hyman"
+        secondNameLabel.text = "Jenny Fleiss"
+    }
+    
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        firstNameLabel.text = ""
+        secondNameLabel.text = ""
+    }
+    
 }
 
